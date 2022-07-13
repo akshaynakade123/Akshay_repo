ef20519fe61cb49b14182674546eb9a22ca6c645
akshay <nakadeakshay977@gmail.com>
2022-07-13 12:52:57 +0530

    ghj

--- sh_1.groovy
+++ sh_1.groovy
@@ -1,13 +1,11 @@
-#// execute echo command
 job('example-1') {
     steps {
         shell('echo Hello World!')
     }
 }
 
-#// read file from workspace
 job('example-2') {
     steps {
-        shell('echo hello >> everyone.txt')
+        shell('echo hello')
     }
 }
